cb8587e7a8c033ab92943d1090de9602e1267672
JosArr <josuedavidarrunategui@gmail.com>
2023-09-26 15:53:11 -0500

    report problem

---
+++ src/components/RepProblema/problem-container-tp.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="problema-container">
+      <h2>Detalles del problema:</h2>
+      <textarea v-model="detallesProblema" rows="4"></textarea>
+      <button class="boton-not"  @click="notificarProblema">Notificar</button>
+    </div>
+</template>
+
+<style>
+  .problema-container {
+    margin-left: 80px;
+    margin-right: 80px;
+  }
+
+  textarea {
+    background-color: #d1cccc;
+    width: 100%;
+    height: 150px;
+    border: none;
+    border-radius: 20px;
+    padding: 10px 20px;
+    margin-top: 10px;
+    cursor: text;
+  }
+
+  .boton-not {
+    background-color: #24A476;
+    color: white;
+    border: none;
+    border-radius: 20px;
+    padding: 10px 20px;
+    margin-top: 10px;
+    cursor: pointer;
+  }
+</style>
+
+<script>
+export default {
+    data() {
+      return {
+      };
+    },
+    methods: {
+        notificarProblema() {
+            console.log("Detalles del problema:", this.detallesProblema);
+            alert('Problema notificado con éxito.');
+            this.detallesProblema = "";
+        },
+    },
+};
+</script>
--- src/views/ReportarProblema.vue
+++ src/views/ReportarProblema.vue
@@ -1,3 +1,34 @@
 <template>
-    <div>Rep problema</div>
-</template>
+    <div>
+      <div>
+        <h1>Selecciona una opción:</h1>
+        <formTP @opcionSeleccionada="handleOpcionSeleccionada" />
+      </div>
+      <div>
+        <btnNotificar/>
+      </div>
+    </div>
+  </template>
+    
+  <script>
+    import formTP from "@/components/RepProblema/form-tp.vue";
+    import btnNotificar from "@/components/RepProblema/problem-container-tp.vue";
+    
+    export default {
+      components: {
+        formTP,
+        btnNotificar,
+      },
+      data() {
+        return {
+          opcionSeleccionada: null,
+          detallesProblema: "",
+        };
+      },
+      methods: {
+        handleOpcionSeleccionada(opcion) {
+          this.opcionSeleccionada = opcion;
+        },
+      },
+    };
+  </script>
