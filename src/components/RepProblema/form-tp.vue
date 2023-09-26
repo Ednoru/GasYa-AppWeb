b92397b60503874810c717a1a08098f37d31cf95
JosArr <josuedavidarrunategui@gmail.com>
2023-09-26 15:33:40 -0500

    a

---
+++ src/components/RepProblema/form-tp.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="tp">
+      <h1>Reportar un problema<br></h1>
+      <ul class="lista-horizontal">
+        <li v-for="(opcion, index) in opciones" :key="index">
+          <button class="boton" :class="{ seleccionado: opcionSeleccionada === opcion }" @click="seleccionarOpcion(opcion)">
+            {{ opcion }}
+          </button>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<style>
+  .boton {
+    background-color: #24A476;
+    color: white;
+    border: none;
+    border-radius: 20px;
+    padding: 10px 20px;
+    margin: 5px;
+    cursor: pointer;
+  }
+
+  .seleccionado {
+    background-color: #d1cccc;
+    color: #24A476;
+  }
+
+  .lista-horizontal {
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+  }
+
+  .tp{
+    margin-top: 20px;
+    margin-left: 80px;
+    margin-right: 80px;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        opciones: ["Precio", "Calificación", "Combustible"],
+        opcionSeleccionada: null,
+      };
+    },
+    methods: {
+      seleccionarOpcion(opcion) {
+        this.opcionSeleccionada = opcion;
+        this.$emit("opcionSeleccionada", opcion);
+      },
+    },
+  };
+</script>
