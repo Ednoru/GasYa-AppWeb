0f4e3029c8d6807cc920fb706fa40570608aa6d9
XLianLZ <Lianbox7304@gmail,com>
2023-09-29 20:05:34 -0500

    commit forms

---
+++ src/components/InicioSesion/inicio-sesion.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <!-- Tu código HTML aquí -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'inicio-sesion',
+    data() {
+      return {
+        // tus datos aquí
+      };
+    },
+    methods: {
+      // tus métodos aquí
+    },
+  }
+  </script>
+  
+  <style scoped>
+  /* Tus estilos aquí */
+  </style>
